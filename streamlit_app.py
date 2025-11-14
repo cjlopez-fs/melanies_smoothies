45cd60d14364d90a2612e09942b794f5468390ad
cjlopez-fs <139356343+cjlopez-fs@users.noreply.github.com>
2025-11-14 16:29:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import pandas as pd
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -49,4 +50,5 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dateframe(data=smoothiefroot_response.json())
+dfp = pd.DataFrame(smoothiefroot_response.json())
+sf_df = st.dateframe(dfp)
